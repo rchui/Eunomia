6e1244897940c60c341cb22497a0c8aa69bb1a63
rchui <ryan.w.chui@gmail.com>
2017-03-13 20:58:48 -0500

    Final comments for today

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -2,15 +2,18 @@ import tensorflow as tf
 import numpy as np
 
 class autoencoder:
+    # Starts a session
     def startSession():
         sess = tf.InteractiveSession()
         init = tf.global_variables_initializer()
         sess.run(init)
         return sess
 
+    # Converts a list into a tensor
     def listToTensor(inputList):
         return tf.convert_to_tensor(inputList, dtype=tf.float32)
 
+    # Prints a tensor
     def printTensor(inputTensor):
         inputTensor = tf.Print(inputTensor, [inputTensor], message = "Printing Tensor: ")
         scratch = tf.add(inputTensor, inputTensor).eval()
