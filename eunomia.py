221739b7fe3a811b88dba7fcc527cc75ba2d8e1b
rchui <ryan.w.chui@gmail.com>
2017-03-14 18:05:41 -0500

    a placeholder and numpy array

--- eunomia.py
+++ eunomia.py
@@ -1,5 +1,6 @@
 import csv
 import tensorflow as tf
+import numpy as np
 from src.autoencoder import autoencoder
 
 # Read in CSV file
@@ -17,18 +18,26 @@ with open("brca_toronto_collab_mutect_123_030617.csv") as csvFile:
         count += 1
 csvFile.close()
 
+# Get the length of brca
 lenBRCA = len(brca[0])
 
+# Convert brca array into numpy array for tensorflow
+inputArray = np.array(brca[0], dtype=tf.float32)
+
 # Initialize weight and adjustment vectors
+a = tf.placeholder(tf.float32, [None, lenBRCA])
 b = tf.Variable(tf.zeros([lenBRCA]))
 W = tf.get_variable('W', shape=[lenBRCA, lenBRCA], initializer = tf.contrib.layers.xavier_initializer())
+
+# Print array dimensions
+print(a.get_shape())
 print(b.get_shape())
 print(W.get_shape())
+
+
 
 # Start tensorflow session
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
-print(b)
-print(W)
 autoencoder.printTensor(b)
 autoencoder.printTensor(W)
