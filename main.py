8c9a3615e86c40a7387b53e82eb32189face9b1c
rchui <ryan.w.chui@gmail.com>
2017-04-03 15:07:42 -0500

    fixed hidden layer constructor func call

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from src.Autoencoder import InputLayer
 inputArray = Utilities.readData()
 
 iLayer = InputLayer(len(inputArray))
-hidden1 = HiddenLAyer(100, iLayer.input)
+hidden1 = HiddenLayer(100, iLayer.input)
 
 sess = Utilities.startSession()
