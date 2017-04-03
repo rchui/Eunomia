df47ff426b73745da5a64512c99ed1a3712e3f24
rchui <ryan.w.chui@gmail.com>
2017-04-03 15:54:47 -0500

    revert self

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ inputArray = Utilities.readData()
 
 iLayer = InputLayer(len(inputArray[1]))
 iLayer.printLayerShape()
-hidden1 = HiddenLayer(100, iLayer.self.input)
+hidden1 = HiddenLayer(100, iLayer.input)
 hidden1.printLayerShape()
-hidden2 = HiddenLayer(50, hidden1.self.z2)
+hidden2 = HiddenLayer(50, hidden1.z2)
 hidden2.printLayerShape()
 
 sess = Utilities.startSession()
