d1ea291ee1d2f48fa25de82eccf7223cea077fd4
rchui <ryan.w.chui@gmail.com>
2017-04-03 11:18:58 -0500

    first object push

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.Utilities import Utilities
+
+test = Utilities()
---
+++ src/Utilities.py
@@ -0,0 +1,27 @@
+import csv
+import random
+import sys
+import tensorflow as tf
+
+class Utilities:
+    def __init__(self):
+        print("Hello World")
+
+    def readData():
+        fileName = sys.argv[1]
+        inputLayer = []
+        with open(fileName) as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                row.pop()
+                if count != 0:
+                    floatRow = [float(i) for i in row]
+                    inputLayer.append(floatRow)
+                count += 1
+        csvFile.close()
+        return inputLayer
+
+    def startSession():
+        sess = tf.InteractiveSession()
+        tf.global_variables_initializer.run()
+        return sess
