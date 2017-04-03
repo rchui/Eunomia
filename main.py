589020e233441165e7ad59818242d6424e090b26
rchui <ryan.w.chui@gmail.com>
2017-04-03 15:47:00 -0500

    printing hidden layer shape

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from src.Autoencoder import HiddenLayer
 inputArray = Utilities.readData()
 
 iLayer = InputLayer(len(inputArray[1]))
+iLayer.printLayerShape()
 hidden1 = HiddenLayer(100, iLayer.input)
-hidden1.buildLayer()
+hidden1.printLayerShape()
 
 sess = Utilities.startSession()
