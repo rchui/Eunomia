61f53e730efcba1a0c2a8495f43822bd93ce6942
rchui <ryan.w.chui@gmail.com>
2017-04-03 15:06:30 -0500

    testing hidden 1 constructor

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from src.Autoencoder import InputLayer
 inputArray = Utilities.readData()
 
 iLayer = InputLayer(len(inputArray))
-iLayer.printLayerShape()
+hidden1 = HiddenLAyer(100, iLayer.input)
 
 sess = Utilities.startSession()
