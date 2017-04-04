c6df2f05bc77379690f6654812f6b76ea1410f88
rchui <ryan.w.chui@gmail.com>
2017-04-03 20:00:08 -0500

    added progress bar

--- src/Utilities.py
+++ src/Utilities.py
@@ -27,4 +27,12 @@ class Utilities:
         tf.global_variables_initializer().run()
         return sess
 
+    def progress(count, total, status=''):
+        bar_len = 60
+        filled_len = int(round(bar_len * count / float(total)))
 
+        percents = round(100.0 * count / float(total), 1)
+        bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+        sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+        sys.stdout.flush()
