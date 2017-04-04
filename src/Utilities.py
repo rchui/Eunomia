6ac45212b30e31d881c5fcbe05728f7cb009c430
rchui <ryan.w.chui@gmail.com>
2017-04-03 20:20:24 -0500

    modified progress bar to new line on finish

--- src/Utilities.py
+++ src/Utilities.py
@@ -33,6 +33,10 @@ class Utilities:
 
         percents = round(100.0 * count / float(total), 1)
         bar = '=' * filled_len + '-' * (bar_len - filled_len)
-
-        sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-        sys.stdout.flush()
+        
+        if count == total: 
+            sys.stdout.write('[%s] %s%s ...%s%s\r' % (bar, percents, '%', status, '\n'))
+            sys.stdout.flush()
+        else:
+            sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+            sys.stdout.flush()
