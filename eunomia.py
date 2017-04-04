2816a3e926c0265d6a97668d47b5b6f5da2b142c
rchui <ryan.w.chui@gmail.com>
2017-04-04 12:06:10 -0500

    reading in real data and printing layer shapes

--- eunomia.py
+++ eunomia.py
@@ -5,63 +5,63 @@ from src.Autoencoder import InputLayer
 from src.Autoencoder import HiddenLayer
 from src.Autoencoder import OutputLayer
 
-import random
-inputArray = []
-for i in range(500):
-    Utilities.progress(i + 1, 1000, status='Building sample ' + str(i + 1))
-    inputInternal = []
-    for j in range(100000):
-        inputInternal.append(random.uniform(0.0, 0.1))
-    inputArray.append(inputInternal)
-for i in range(500):
-    Utilities.progress(i + 501, 1000, status='Building sample ' + str(i + 501))
-    inputInternal = []
-    for j in range(100000):
-        inputInternal.append(random.uniform(0.9, 1.0))
-    inputArray.append(inputInternal)
-random.shuffle(inputArray)
+# import random
+# inputArray = []
+# for i in range(500):
+    # Utilities.progress(i + 1, 1000, status='Building sample ' + str(i + 1))
+    # inputInternal = []
+    # for j in range(100000):
+        # inputInternal.append(random.uniform(0.0, 0.1))
+    # inputArray.append(inputInternal)
+# for i in range(500):
+    # Utilities.progress(i + 501, 1000, status='Building sample ' + str(i + 501))
+    # inputInternal = []
+    # for j in range(100000):
+        # inputInternal.append(random.uniform(0.9, 1.0))
+    # inputArray.append(inputInternal)
+# random.shuffle(inputArray)
 
 # Read in data from csv file
 Utilities.progress(1, 7, status='Reading in data        ')
-# inputArray = Utilities.readData()
-
+inputArray = Utilities.readData()
 
 # Build input layer
 Utilities.progress(2, 7, status='Building input layer   ')
 with tf.variable_scope("input"):
     iLayer = InputLayer(len(inputArray[1]))
-# iLayer.printLayerShape()
 
 # Build hidden layer 1
 Utilities.progress(3, 7, status='Building hidden layer 1')
 with tf.variable_scope("hidden1"):
     hidden1 = HiddenLayer(100, iLayer.inputLayer)
     hidden1.buildTrainer()
-# hidden1.printLayerShape()
 
 # Build hidden layer 2
 Utilities.progress(4, 7, status='Building hidden layer 2')
 with tf.variable_scope("hidden2"):
     hidden2 = HiddenLayer(50, hidden1.y1)
     hidden2.buildTrainer()
-# hidden2.printLayerShape()
 
 # Build hidden layer 3
 Utilities.progress(5, 7, status='Building hidden layer 3')
 with tf.variable_scope("hidden3"):
     hidden3 = HiddenLayer(16, hidden2.y1)
     hidden3.buildTrainer()
-# hidden3.printLayerShape()
 
 # Build output layer
 Utilities.progress(6, 7, status='Building output layer  ')
 with tf.variable_scope("output"):
     oLayer = OutputLayer(2, hidden3.y1)
     oLayer.buildTrainer()
-# oLayer.printLayerShape()
 
 Utilities.progress(7, 7, status='Starting session       ')
 sess = Utilities.startSession()
+
+iLayer.printLayerShape()
+hidden1.printLayerShape()
+hidden2.printLayerShape()
+hidden3.printLayerShape()
+oLayer.printLayerShape()
 
 # Training the hidden layers and output layer on the data.
 for j in range(10):
