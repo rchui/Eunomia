a195c962688f391a522db95a90f054ab3d07cabb
rchui <ryan.w.chui@gmail.com>
2017-04-03 22:07:40 -0500

    fully commented Utilities.py

--- src/Utilities.py
+++ src/Utilities.py
@@ -5,6 +5,7 @@ import numpy as np
 
 class Utilities:
     def readData():
+        """ Reads in data passed by the user from a CSV file. """
         count = 0
         fileName = sys.argv[1]
         inputArray = []
@@ -20,14 +21,25 @@ class Utilities:
         return inputArray
 
     def numpyReshape(array):
+        """ Reshapes a given array to match tensor dimensions.
+        @params:
+            array -- the array to be reshaped and turned into a numpy array
+        """
         return np.array(array, dtype = float).reshape(1, len(array))
 
     def startSession():
+        """ Starts an interactive session to run the tensorflow graph. """
         sess = tf.InteractiveSession()
         tf.global_variables_initializer().run()
         return sess
 
     def progress(count, total, status=''):
+        """ Displays a progress bar that updates with the program's progress.
+        @params:
+            count -- distance traveled so far
+            total -- total distance to travel
+            status -- updates on the progress of the program
+        """
         bar_len = 60
         filled_len = int(round(bar_len * count / float(total)))
 
