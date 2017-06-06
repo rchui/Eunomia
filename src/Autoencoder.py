9e3001cf3da358861934c6e78be9b0fb45914592
rchui <ryan.w.chui@gmail.com>
2017-06-06 17:15:50 -0500

    changed sparsity function

--- src/Autoencoder.py
+++ src/Autoencoder.py
@@ -53,9 +53,9 @@ class HiddenLayer:
         self.rho = rho
 
         self.squareDifference = tf.reduce_sum(tf.square(self.layerInput - self.y2))
-        self.rhoHat = tf.reduce_mean(tf.divide(tf.reduce_sum(self.y1, 1), self.outSize))
-        self.sparsity = alpha * (self.rho * tf.log(tf.divide(self.rho, self.rhoHat)) + 
-                            (1 - self.rho) * tf.log(tf.divide((1 - self.rho), (1 - self.rhoHat))))
+        self.rhoHat = tf.divide(tf.reduce_sum(self.y1, 0), self.outSize)
+        self.sparsity = alpha * tf.reduce_sum(tf.add(tf.multiply(self.rho, tf.log(tf.divide(self.rho, self.rhoHat))), 
+                                                                 tf.multiply(tf.subtract(1, self.rho), tf.log(tf.divide(tf.subtract(1, self.rho), tf.subtract(1, self.rhoHat))))))
         self.l2 = beta * tf.nn.l2_loss(self.w1) + beta * tf.nn.l2_loss(self.w2)
         self.loss = self.squareDifference + self.l2 + self.sparsity
         self.trainStep = tf.train.AdamOptimizer().minimize(self.loss)
