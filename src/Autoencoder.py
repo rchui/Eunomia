193de339d853e54dcf260c7bf900029f07b39fb1
rchui <ryan.w.chui@gmail.com>
2017-06-06 17:50:41 -0500

    add sparsity

--- src/Autoencoder.py
+++ src/Autoencoder.py
@@ -57,8 +57,10 @@ class HiddenLayer:
         self.rhoHat = tf.divide(tf.reduce_sum(self.y1, 1), self.outSize)
         self.rhoHat = tf.Print(self.rhoHat, [self.rhoHat], summarize = 5)
         print(self.rhoHat.get_shape().as_list())
-        self.sparsity = alpha * (self.rho * tf.log(tf.divide(self.rho, self.rhoHat)) + 
-                            (1 - self.rho) * tf.log(tf.divide((1 - self.rho), (1 - self.rhoHat))))
+
+        self.sparsity = alpha * tf.reduce_sum(tf.add(tf.multiply(self.rho, tf.log(tf.divide(self.rho, self.rhoHat))),
+        tf.multiply(tf.subtract(1.0, self.rho), tf.log(tf.divide(tf.subtract(1.0, self.rho), tf.subtract(1.0, self.rhoHat))))))
+
         self.l2 = beta * tf.nn.l2_loss(self.w1) + beta * tf.nn.l2_loss(self.w2)
         self.loss = self.squareDifference + self.l2 + self.sparsity
         self.trainStep = tf.train.AdamOptimizer().minimize(self.loss)
