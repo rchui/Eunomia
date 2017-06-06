df71f9bf1701c8c3f75b61f29b1a223d782884aa
rchui <ryan.w.chui@gmail.com>
2017-06-06 10:48:51 -0500

    100 epochs

--- eunomia.py
+++ eunomia.py
@@ -7,7 +7,7 @@ from src.Autoencoder import HiddenLayer
 from src.Autoencoder import OutputLayer
 
 # Number of epochs to run
-numEpochs = 250
+numEpochs = 100
 # Size of each batch
 batchSize = 10
 # Scaling factor for sparsity cost function
