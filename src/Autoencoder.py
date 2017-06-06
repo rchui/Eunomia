834112d944789a48db1ba95ff181f472b9512859
rchui <ryan.w.chui@gmail.com>
2017-06-06 11:50:22 -0500

    swap to tf.log

--- src/Autoencoder.py
+++ src/Autoencoder.py
@@ -54,8 +54,8 @@ class HiddenLayer:
 
         self.squareDifference = tf.reduce_sum(tf.square(self.layerInput - self.y2))
         self.rhoHat = tf.reduce_mean(tf.divide(tf.reduce_sum(self.y1, 1), self.outSize))
-        self.sparsity = alpha * (self.rhoHat * np.log(self.rho / self.rhoHat) + 
-                            (1 - self.rho) * np.log((1 - self.rho) / (1 - self.rhoHat)))
+        self.sparsity = alpha * (self.rhoHat * tf.log(self.rho / self.rhoHat) + 
+                            (1 - self.rho) * tf.log((1 - self.rho) / (1 - self.rhoHat)))
         self.l2 = beta * tf.nn.l2_loss(self.w1) + beta * tf.nn.l2_loss(self.w2)
         self.loss = self.squareDifference + self.l2 + self.sparsity
         self.trainStep = tf.train.AdamOptimizer().minimize(self.loss)
