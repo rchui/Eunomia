91c02d4ab37f2452eece6f0733dcf6ae556852ad
rchui <ryan.w.chui@gmail.com>
2017-06-06 17:49:03 -0500

    reduce sume across column axis

--- src/Autoencoder.py
+++ src/Autoencoder.py
@@ -54,7 +54,7 @@ class HiddenLayer:
         self.rho = rho
 
         self.squareDifference = tf.reduce_sum(tf.square(self.layerInput - self.y2))
-        self.rhoHat = tf.divide(tf.reduce_sum(self.y1, 0), self.outSize)
+        self.rhoHat = tf.divide(tf.reduce_sum(self.y1, 1), self.outSize)
         self.rhoHat = tf.Print(self.rhoHat, [self.rhoHat], summarize = 5)
         print(self.rhoHat.get_shape().as_list())
         self.sparsity = alpha * (self.rho * tf.log(tf.divide(self.rho, self.rhoHat)) + 
