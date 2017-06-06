4390642a461f8321f172c98d14d83bfb307d49eb
rchui <ryan.w.chui@gmail.com>
2017-06-06 14:31:43 -0500

    checking dictFeeder

--- src/Utilities.py
+++ src/Utilities.py
@@ -35,7 +35,9 @@ class Utilities:
         for i in range(numBatches):
             start = i * batchSize
             dictFeeder.append(array[start:start + batchSize])
-        dictFeeder.append(array[(numBatches * batchSize):(len(array) - 1)])
+        print(numBatches * batchSize)
+        print(len(array))
+        dictFeeder.append(array[(numBatches * batchSize):(len(array))])
         return dictFeeder
 
     def numpyReshape(array):
