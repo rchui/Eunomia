7aa4a238fa1a4d88f31720d7134cf963927ea619
rchui <ryan.w.chui@gmail.com>
2017-06-06 23:31:10 -0500

    summarize to 16

--- src/Autoencoder.py
+++ src/Autoencoder.py
@@ -55,7 +55,7 @@ class HiddenLayer:
 
         self.squareDifference = tf.reduce_sum(tf.square(self.layerInput - self.y2))
         self.rhoHat = tf.divide(tf.reduce_sum(self.y1, 0), self.outSize)
-        self.rhoHat = tf.Print(self.rhoHat, [self.rhoHat], summarize = 5)
+        self.rhoHat = tf.Print(self.rhoHat, [self.rhoHat], summarize = 16)
 
         self.sparsity = alpha * tf.reduce_sum(tf.add(tf.multiply(self.rho, 
                                 tf.log(tf.divide(self.rho, self.rhoHat))),
